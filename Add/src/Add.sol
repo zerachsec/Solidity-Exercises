f01854f37cf377e87d655a3c53fb34fc7a2d1e59
iVishaal$ <ivishaal@iVishaals-MacBook-Pro.local>
2025-09-04 08:44:05 +0530

    add

--- Add/src/Add.sol
+++ Add/src/Add.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 contract Add {
     function add(uint256 a, uint256 b) public pure returns (uint256) {
-        // your code here
+        uint256 sum = a + b;
+        return sum;
     }
 }
